c9160e462ed7a6976c7a15e1a91bc8be79ff7823
Hugo <hugopf.mail@gmail.com>
2019-02-11 23:43:03 +0100

    [FIX] Areglo de Bucle 5
    Arreglo de Bucle5 por el que se entraba en un bucle infinito

--- src/alg77777777/p1/Bucle5.java
+++ src/alg77777777/p1/Bucle5.java
@@ -1,12 +1,13 @@
 package alg77777777.p1;
 
 public class Bucle5 {
+
     public static long bucle5(int n) {
         long cont = 0;
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++)
-                for (int k = 0; k < n; k++)
-                    for (int l = 0; l < n; l*=2)
+                for (int k = 1; k <= n; k++)
+                    for (int l = 1; l <= n; l *= 2)
                         cont++;
         return cont;
 
@@ -16,7 +17,7 @@ public class Bucle5 {
         long c = 0;
         long t1, t2;
         int nVeces = Integer.parseInt(arg[0]);
-
+        System.out.println("n\ttiempo\tcontador");
         for (int n = 8; n <= 100000; n *= 2) {
             t1 = System.currentTimeMillis();
 
