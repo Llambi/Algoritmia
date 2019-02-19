905a44d06d65ed411c478d9b3f4835c9a33b97a7
Hugo Perez <hugopf.mail@gmail.com>
2019-02-19 16:17:43 +0100

    [ADD] Insercion
    Añadida la funcionalidad para la ordenacion por algoritmo de insercion

--- src/alg77777777/p2/Insercion.java
+++ src/alg77777777/p2/Insercion.java
@@ -1,29 +1,47 @@
-package alg77777777.p2 ;
+package alg77777777.p2;
 
-import alg77777777.p2.Vector;
-
-/** Este programa sirve para ordenar n elementos
-	con un algoritmo de los "malos" (cuadrático)· 
-	Es la INSERCIÓN DIRECTA
+/**
+ * Este programa sirve para ordenar n elementos
+ * con un algoritmo de los "malos" (cuadrático)·
+ * Es la INSERCIÓN DIRECTA
  */
-public class Insercion extends Vector
-{
-	
-	public Insercion(int nElementos) {
-		super(nElementos);
-	}
+public class Insercion extends Vector {
 
-	/**
-	 * Ordenación por inserción directa
-	 */
+    public Insercion(int nElementos) {
+        super(nElementos);
+    }
 
-	@Override
-	public void ordenar() {
-		// TODO: Implementación del método de ordenación
-	} 
+    /**
+     * Ordenación por inserción directa
+     */
 
-	@Override
-	public String getNombre() {
-		return "Inserción directa";
-	} 
-} 
+    @Override
+    public void ordenar() {
+        int tam = elements.length;
+        int pos = 1;
+
+        while (pos < tam) {
+            if (elements[pos] < elements[pos - 1]) {
+                int element = pos;
+                int previous = pos - 1;
+                do {
+                    permut(elements, element, previous);
+                    element--;
+                    previous--;
+                } while (previous >= 0 && elements[element] < elements[previous]);
+            }
+            pos++;
+        }
+    }
+
+    private void permut(int[] lista, int element, int previous) {
+        int aux = lista[element];
+        lista[element] = lista[previous];
+        lista[previous] = aux;
+    }
+
+    @Override
+    public String getNombre() {
+        return "Inserción directa";
+    }
+}
