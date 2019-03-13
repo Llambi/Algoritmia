901b40d4562ed8ca86f0d79a423da8af43e1400b
Hugo Perez <hugopf.mail@gmail.com>
2019-03-13 16:46:14 +0100

    [ADD] Añadida implementacion para la toma de tiempos.

--- src/alg77777777/p31/EnteroGrandePrueba.java
+++ src/alg77777777/p31/EnteroGrandePrueba.java
@@ -5,23 +5,24 @@ import java.math.BigInteger;
 public class EnteroGrandePrueba {
 
     public static void main(String[] args) {
-        pruebaSimple();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Prueba " + i);
-
-            int c = 1024; // num cifras
+        int c = 16;
+        int n = 100;
+        for (int i = 0; i < 100000000; i++) {
             EnteroGrande ent1 = EnteroGrande.generar(c);
             EnteroGrande ent2 = EnteroGrande.generar(c);
-            System.out.println("Aleatorio de " + c + " cifras: \t" + ent1.mostrarVista());
-            System.out.println("Aleatorio de " + c + " cifras: \t" + ent2.mostrarVista());
-
-            pruebaMultiplicacionClasica(ent1, ent2);
-			pruebaMultiplicacionDV(ent1, ent2);
-
-
+            long init = System.currentTimeMillis();
+            for (int j = 0; j < n; j++)
+                pruebaMultiplicacionClasica(ent1, ent2);
+            long end = System.currentTimeMillis();
+            long iter = end - init;
+            init = System.currentTimeMillis();
+            for (int j = 0; j < n; j++)
+                pruebaMultiplicacionDV(ent1, ent2);
+            end = System.currentTimeMillis();
+            long div = end - init;
+            System.out.println(c + "\t" + iter + "\t" + div);
+            c *= 2;
         }
-        System.out.println("Fin pruebas");
-
     }
 
     private static void pruebaSimple() {
@@ -39,19 +40,19 @@ public class EnteroGrandePrueba {
         BigInteger bi1 = new BigInteger(ent1.toString());
         BigInteger bi2 = new BigInteger(ent2.toString());
         BigInteger bires = bi1.multiply(bi2);
-        if (!bires.toString().equals(res.toString()))
-            System.err.println("Error en la mult Entero1=" + ent1 + " * Entero2=" + ent2 + "********************");
-        System.out.println("Mult Entero1 * Entero2=\t" + res);
-        System.out.println("BigInteger multiply=\t" + bires.toString());
+//        if (!bires.toString().equals(res.toString()))
+//            System.err.println("Error en la mult Entero1=" + ent1 + " * Entero2=" + ent2 + "********************");
+//        System.out.println("Mult Entero1 * Entero2=\t" + res);
+//        System.out.println("BigInteger multiply=\t" + bires.toString());
     }
 
     private static void pruebaMultiplicacionDV(EnteroGrande ent1, EnteroGrande ent2) {
         EnteroGrande res = ent1.multiplicarDV(ent2);
         BigInteger aux = new BigInteger(ent1.toString()).multiply(new BigInteger(ent2.toString()));
         EnteroGrande result = new EnteroGrande(aux.toString());
-        if (res.compareTo(result) != 0)
-            System.err.println("Error en la mult Entero1= " + ent1 + " * Entero2= " + ent2 + "********************");
-        System.out.println("Mult Entero1 * Entero2= \t" + res.mostrarVista());
+//        if (res.compareTo(result) != 0)
+//            System.err.println("Error en la mult Entero1= " + ent1 + " * Entero2= " + ent2 + "********************");
+//        System.out.println("Mult Entero1 * Entero2= \t" + res.mostrarVista());
 
     }
 }
